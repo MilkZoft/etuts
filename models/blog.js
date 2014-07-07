400dc308ab22785045bc6cbe3415ae83392a257f
Carlos Santana Roldán <carlos@milkzoft.com>
2014-07-07 16:41:17 -0700

    getPostByCategory added

--- models/blog.js
+++ models/blog.js
@@ -1,6 +1,6 @@
 var Model = require('../system/model'),
     Blog = new Model();
-    fields = 'id, title, slug, excerpt, content, author, mainImage, createdAt, day, month, year, language';
+    fields = 'blog_posts.id, title, blog_posts.slug, excerpt, content, author, mainImage, createdAt, day, month, year, blog_posts.language';
 
 module.exports = {
   getAll: function(callback) {
@@ -15,7 +15,25 @@ module.exports = {
     Blog.query(sql, callback);
   },
 
-  getPostByDate: function(params, callback) {
+  getPostsByCategory: function(params, callback) {
+    var language = global.lang.current,
+        categories = ", GROUP_CONCAT(blog_categories.category SEPARATOR ', ') AS categories",
+        sql = "SELECT " + fields + categories +
+              " FROM blog_posts " + 
+              " LEFT JOIN blog_re_categories2posts ON (blog_re_categories2posts.postId = blog_posts.id)" +
+              " LEFT JOIN blog_categories ON (blog_categories.id = blog_re_categories2posts.categoryId)" +
+              " WHERE blog_categories.slug = '" + params.category + "'" +
+              " AND blog_posts.language = '" + language + "'" + 
+              " AND blog_categories.language = '" + language + "'" + 
+              " GROUP BY blog_posts.id"
+              " ORDER BY blog_posts.id DESC" +
+              " LIMIT 0, 12";
+        console.log(sql);
+       
+    Blog.query(sql, callback);
+  },
+
+  getPostsByDate: function(params, callback) {
     var language = global.lang.current,
         sql = "SELECT " + fields + 
               " FROM blog_posts " + 
